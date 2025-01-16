c2030807c412ac1c9b4ba7fc94d14e2c81cfab65
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2025-01-16 15:19:21 +0530

    fixed some access issues

--- app.js
+++ app.js
@@ -27,7 +27,13 @@ mongoose
   .catch((err) => console.error(err));
 
 // Middleware
-app.use(cors());
+// Allow requests from specific origin
+const corsOptions = {
+  origin: 'http://localhost:5173', // Change this to your client URL
+  methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(fileUpload({
   useTempFiles: true
